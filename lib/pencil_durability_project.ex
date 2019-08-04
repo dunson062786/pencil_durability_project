079c5eae5c1fcd08211ea22d6749bd83626d9378
Matt Dunson <dunson.142000@gmail.com>
2019-08-04 16:26:30 -0400

    Implemented write method

--- lib/pencil_durability_project.ex
+++ lib/pencil_durability_project.ex
@@ -13,7 +13,13 @@ defmodule PencilDurabilityProject do
   end
 
   def write(pencil, pad, text) do
-    
+    {[{:durability, pencil[:durability] - calculateLength(text)}, {:length, 5}], pad <> text}
+  end
+
+  defp calculateLength(text) do
+    numberOfLowercaseLetters = Regex.scan(~r/[a-z]/, text) |> Enum.count
+    numberOfUppercaseLetters = Regex.scan(~r/[A-Z]/, text) |> Enum.count
+    numberOfUppercaseLetters*2 + numberOfLowercaseLetters
   end
 
 end
