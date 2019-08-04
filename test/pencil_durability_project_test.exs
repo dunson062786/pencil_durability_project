f412d494c65e393ed7d724babb7e0d114a4fc432
Matt Dunson <dunson.142000@gmail.com>
2019-08-04 16:25:35 -0400

    corrected title of test case and added case for capital letters

--- test/pencil_durability_project_test.exs
+++ test/pencil_durability_project_test.exs
@@ -2,10 +2,18 @@ defmodule PencilDurabilityProjectTest do
   use ExUnit.Case
   doctest PencilDurabilityProject
 
-  test "greets the world" do
+  test "writes text and decreases durability of pencil" do
     pencil = [{:durability, 40_000}, {:length, 5}]
     pad = ""
     text = "text"
     assert PencilDurabilityProject.write(pencil, pad, text) == {[{:durability, 39_996}, {:length, 5}], "text"}
   end
+
+  test "write works on capital letters too" do
+    pencil = [{:durability, 40_000}, {:length, 5}]
+    pad = ""
+    text = "aaAb"
+    assert PencilDurabilityProject.write(pencil, pad, text) == {[{:durability, 39_995}, {:length, 5}], "aaAb"}
+  end
+
 end
