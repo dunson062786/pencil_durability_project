9b8d8c688e9f1ecf789f58c171608178bf501917
Matt Dunson <dunson.142000@gmail.com>
2019-08-04 16:42:33 -0400

    added max_durability to pencil object

--- test/pencil_durability_project_test.exs
+++ test/pencil_durability_project_test.exs
@@ -3,17 +3,17 @@ defmodule PencilDurabilityProjectTest do
   doctest PencilDurabilityProject
 
   test "writes text and decreases durability of pencil" do
-    pencil = [{:durability, 40_000}, {:length, 5}]
+    pencil = [{:max_durability, 40_000}, {:durability, 40_000}, {:length, 5}]
     pad = ""
     text = "text"
-    assert PencilDurabilityProject.write(pencil, pad, text) == {[{:durability, 39_996}, {:length, 5}], "text"}
+    assert PencilDurabilityProject.write(pencil, pad, text) == {[{:max_durability, 40_000}, {:durability, 39_996}, {:length, 5}], "text"}
   end
 
   test "write works on capital letters too" do
-    pencil = [{:durability, 40_000}, {:length, 5}]
+    pencil = [{:max_durability, 40_000}, {:durability, 40_000}, {:length, 5}]
     pad = ""
     text = "aaAb"
-    assert PencilDurabilityProject.write(pencil, pad, text) == {[{:durability, 39_995}, {:length, 5}], "aaAb"}
+    assert PencilDurabilityProject.write(pencil, pad, text) == {[{:max_durability, 40_000}, {:durability, 39_995}, {:length, 5}], "aaAb"}
   end
 
 end
