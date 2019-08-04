c13e40bfed4e249923c9d2d744fac97aed6d2c35
Matt Dunson <dunson.142000@gmail.com>
2019-08-04 14:08:54 -0400

    Initial Mix Application

---
+++ lib/pencil_durability_project.ex
@@ -0,0 +1,18 @@
+defmodule PencilDurabilityProject do
+  @moduledoc """
+  Documentation for PencilDurabilityProject.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> PencilDurabilityProject.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/pencil_durability_project_test.exs
@@ -0,0 +1,8 @@
+defmodule PencilDurabilityProjectTest do
+  use ExUnit.Case
+  doctest PencilDurabilityProject
+
+  test "greets the world" do
+    assert PencilDurabilityProject.hello() == :world
+  end
+end
