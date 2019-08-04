5ef70e7f0b4baa616912e4dccd81c8c91c8ff3da
Matt Dunson <dunson.142000@gmail.com>
2019-08-04 15:45:53 -0400

    Added unit test for write function

--- test/pencil_durability_project_test.exs
+++ test/pencil_durability_project_test.exs
@@ -3,6 +3,9 @@ defmodule PencilDurabilityProjectTest do
   doctest PencilDurabilityProject
 
   test "greets the world" do
-    assert PencilDurabilityProject.hello() == :world
+    pencil = [{:durability, 40_000}, {:length, 5}]
+    pad = ""
+    text = "text"
+    assert PencilDurabilityProject.write(pencil, pad, text) == {[{:durability, 39_996}, {:length, 5}], "text"}
   end
 end
