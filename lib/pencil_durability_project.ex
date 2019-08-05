136691fb3420ca0b693ddd8df1c7de9b2a0273d7
Matt Dunson <dunson.142000@gmail.com>
2019-08-04 20:49:01 -0400

    Added prefix function and case to write function to handle a pencil that has gone dull

--- lib/pencil_durability_project.ex
+++ lib/pencil_durability_project.ex
@@ -15,13 +15,35 @@ defmodule PencilDurabilityProject do
   end
 
   def write(pencil, pad, text) do
-    {[{:max_durability, pencil[:max_durability]}, {:durability, pencil[:durability] - calculateLength(text)}, {:length, 5}], pad <> text}
+    cond do
+      calculateLength(text) > pencil[:durability] -> {
+        [
+          {:max_durability, pencil[:max_durability]}, 
+          {:durability, pencil[:durability] - calculateLength(prefix(text, pencil[:durability]))}, 
+          {:length, pencil[:length]}
+        ], 
+        pad <> String.pad_trailing(prefix(text, pencil[:durability]), String.length(text))
+      }
+      true -> {[{:max_durability, pencil[:max_durability]}, {:durability, pencil[:durability] - calculateLength(text)}, {:length, pencil[:length]}], pad <> text}
+    end
   end
 
   defp calculateLength(text) do
     numberOfLowercaseLetters = Regex.scan(~r/[a-z]/, text) |> Enum.count
     numberOfUppercaseLetters = Regex.scan(~r/[A-Z]/, text) |> Enum.count
     numberOfUppercaseLetters*2 + numberOfLowercaseLetters
+  end
+
+  defp prefix(text, length) do
+    first_letter = String.at(text, 0)
+    first_letter_length = calculateLength(first_letter)
+    cond do
+      length == 0 -> ""
+      length == 1 && first_letter_length == 1 -> first_letter
+      length == 1 && first_letter_length == 2 -> ""
+      length == 2 && first_letter_length == 2 -> first_letter
+      true -> "#{first_letter}#{prefix(String.slice(text, 1..-1), length-first_letter_length)}"
+    end
   end
 
 end
